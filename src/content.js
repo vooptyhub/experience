ed65e558123ec4386f95ef8847c409497d766470
karthikveluri <kveluri@eab.com>
2019-08-06 11:25:10 -0400

    Updated address for most of the places

--- src/content.js
+++ src/content.js
@@ -11,8 +11,8 @@ export const Content = [
         name: 'House Of Cards',
         shortDescription: 'Let the Underwoods be your guide around DC',
         description: 'Explore DC with iconic Netflix series! Let the Underwoods guide you around DC. Learn from masters how to deal with ruthless pragmatism, manipulation, betrayal, and power. Visit the places where the series were shut.',
-        dinner: 'Federalist pig',
-        drink: 'Lion',
+        dinner: 'Federalist pig (1654 Columbia Rd NW, Washington, DC 20009)',
+        drink: 'Lion (1823 L St NW, Washington, DC 20036)',
         location: 'Row houses along T Street in NW'
     },
     {
@@ -20,36 +20,36 @@ export const Content = [
         name: 'Blagden Alley',
         shortDescription: 'Exerience the Instagram Paradise',
         description: 'Grab a friend, or two or three or four and explore few of these beautiful spots while taking those pictures for the gram. Start your evening with a classy cocktail, enjoy a meal that explodes your taste buds and finally cool off with a tasty desert',
-        dinner: 'Espita Mezcaleria',
-        drink: 'Columbia Room',
-        location: 'Walk around Blagden Alley, its an instagram paradise'
+        dinner: 'Smoked Lamb shoulder @ Espita Mezcaleria (1250 9th St NW, Washington, DC 20001)',
+        drink: 'Grab unique cocktails @ Columbia Room (124 Blagden Alley NW, Washington, DC 20001)',
+        location: 'Walk around Blagden Alley, its an instagram paradise (Blagden Alley Northwest, Washington, DC)'
     },
     {
         img: dcGay,
         name: 'The Rainbow',
         shortDescription: 'Be Gay in DC',
         description: 'Washington, DC is one of the most diverse – and welcoming – cities in the world. At once an international crossroads and an all-American hometown, the District is home to people and preferences from every color of the rainbow. Tour historic LGBTQ sites, grab a drink at the best gay and lesbian bars in the city, rock out at the Capital Pride Parade and Concert and so much more. Make your DC adventure all your own.',
-        dinner: 'Annie’s',
-        drink: 'Number 9 Lounge Bar',
-        location: 'Visit Hirshhorn Museum and do some shopping at City Center'
+        dinner: 'Annie’s (1609 17th St NW, Washington, DC 20009)',
+        drink: 'Number 9 Lounge Bar (1435 P St NW, Washington, DC 20005)',
+        location: 'Visit Hirshhorn Museum and do some shopping at City Center(Independence Ave SW &, 7th St SW, Washington, DC 20560)'
     },
     {
         img: Georgetown,
         name: 'Georgetown',
         shortDescription: 'Explore Georgetown while enjoying this experience',
         description: 'Georgetown is a charming area with Federal-style architecture, cobblestone streets and fashion and design shops. The dining scene is defined by upmarket restaurants and waterfront seafood spots, while nightlife spans boisterous college bars, traditional taverns and intimate live music lounges. Georgetown Waterfront Park has a riverside promenade and gardens, and there\'s a bike path along the C&O Canal.',
-        dinner: 'Filomena',
-        drink: 'Cup cake from Baked and wired',
-        location: 'Grab a drink from The Alex'
+        dinner: 'Satisfy that Italian food craving @ Filomena (1063 Wisconsin Ave NW, Washington, DC 20007)',
+        drink: 'Cup cake from Baked and wired (1052 Thomas Jefferson St NW, Washington, DC 20007)',
+        location: 'Grab a drink from The Alex (1075 Thomas Jefferson St NW, Washington, DC 20007)'
     },
     {
         img: russianSpy,
         name: 'Russian Spy',
         shortDescription: 'Explore DC like a russian spy',
         description: 'Explore DC like a russian spy. Go where they go, drink what they drunk, learn how to be invisible in public.',
-        dinner: 'Russia house',
-        drink: 'Mari Vanna',
-        location: 'Spy museum'
+        dinner: 'Russia house (1800 Connecticut Ave NW, Washington, DC 20009)',
+        drink: 'Mari Vanna (1141 Connecticut Ave NW, Washington, DC 20036)',
+        location: 'Spy museum (700 L\'Enfant Plaza SW, Washington, DC 20024)'
     },
 	{
         img: Zoo ,
